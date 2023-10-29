f5898c3b865eefb1d419ba2d90dddde3ca295a0f
Dan <dan@manby.org>
2023-10-29 10:18:54 +0000

    Changed how minimax nodes deal with memory allocation.

--- TurnBasedAICPP/AI/MinimaxAI.cpp
+++ TurnBasedAICPP/AI/MinimaxAI.cpp
@@ -4,14 +4,16 @@
 using std::max;
 using std::min;
 
-Action MinimaxAI::GetMove()
+Action MinimaxAI::GetAction() const
 {
-	Node node = Node(m_State);
+	Node node(m_State);
+	node.Minimax(3, true);
 	return node.GetBestAction();
 }
-int MinimaxAI::Node::Minimax(int depth, int alpha, int beta, bool maximiser)
+
+int MinimaxAI::Node::Minimax(int depth, bool maximiser, int alpha, int beta)
 {
-	if (depth == 0 || m_State.IsGameOver()) return getEvaluation();
+	if (depth == 0 || m_State->IsGameOver()) return getEvaluation();
 	populateActions();
 	if (m_Actions.empty()) return getEvaluation();
 
@@ -21,11 +23,11 @@ int MinimaxAI::Node::Minimax(int depth, int alpha, int beta, bool maximiser)
 	{
 		int best = INT32_MIN;
 		for (int i = 0; i < m_Children.size(); i++) {
-			auto& childNode = m_Children[i];
-			int childScore = childNode.Minimax(depth - 1, alpha, beta, false);
+			const auto& childNode = m_Children[i];
+			int childScore = childNode->Minimax(depth - 1, false, alpha, beta);
 			if (childScore > best) {
 				best = childScore;
-				m_BestAction = m_Actions[i];
+				m_BestAction = *m_Actions[i];
 			}
 			alpha = max(alpha, best);
 			if (alpha >= beta) break;
@@ -37,11 +39,11 @@ int MinimaxAI::Node::Minimax(int depth, int alpha, int beta, bool maximiser)
 	{
 		int best = INT32_MAX;
 		for (int i = 0; i < m_Children.size(); i++) {
-			auto& childNode = m_Children[i];
-			int childScore = childNode.Minimax(depth - 1, alpha, beta, true);
+			const auto& childNode = m_Children[i];
+			int childScore = childNode->Minimax(depth - 1, true, alpha, beta);
 			if (childScore < best) {
 				best = childScore;
-				m_BestAction = m_Actions[i];
+				m_BestAction = *m_Actions[i];
 			}
 			beta = min(beta, best);
 			if (beta <= alpha) break;
@@ -53,31 +55,61 @@ int MinimaxAI::Node::Minimax(int depth, int alpha, int beta, bool maximiser)
 
 int MinimaxAI::Node::getEvaluation() const
 {
-	if ((User)m_State.GetResult() == m_State.GetPlayer()) return INT32_MAX;
-	if ((User)m_State.GetResult() == m_State.GetEnemy())  return INT32_MIN;
+	if ((User)m_State->GetResult() == m_State->GetPlayer()) return INT32_MAX;
+	if ((User)m_State->GetResult() == m_State->GetEnemy())  return INT32_MIN;
 
 	int playerKingHealthPool = 0;
 	int enemyKingHealthPool = 0;
-	const auto& kings = m_State.GetKings();
+	const auto& kings = m_State->GetKings();
+
+	/*const std::array<Vec2, 8> directionVectors = { {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, -1}, {1, 1}, {-1, 1}, {-1, -1}} };
+
+	int playerFreeAdjacentSquares = 0;
+	int enemyFreeAdjacentSquares = 0;*/
+
 	for (const auto& king : kings)
 	{
-		if (king.GetOwner() == m_State.GetPlayer()) playerKingHealthPool += king.GetHealth();
-		if (king.GetOwner() == m_State.GetEnemy())  enemyKingHealthPool  += king.GetHealth();
+		if (king.GetOwner() == m_State->GetPlayer())
+		{
+			playerKingHealthPool += king.GetHealth();
+			//for (const auto& vector : directionVectors)
+			//{
+			//	const int x = king.GetX() + vector.X;
+			//	const int y = king.GetY() + vector.Y;
+			//	if (!GameState::IsInBounds(x, y) || !m_State->IsPassable(x, y)) continue;
+			//	playerFreeAdjacentSquares++;
+			//}
+		}
+		if (king.GetOwner() == m_State->GetEnemy())
+		{
+			enemyKingHealthPool += king.GetHealth();
+			//for (const auto& vector : directionVectors)
+			//{
+			//	const int x = king.GetX() + vector.X;
+			//	const int y = king.GetY() + vector.Y;
+			//	if (!GameState::IsInBounds(x, y) || !m_State->IsPassable(x, y)) continue;
+			//	enemyFreeAdjacentSquares++;
+			//}
+		}
 	}
 
-	return playerKingHealthPool - enemyKingHealthPool;
+	return (enemyKingHealthPool - playerKingHealthPool);// + (enemyFreeAdjacentSquares - playerFreeAdjacentSquares);
 }
 
 void MinimaxAI::Node::populateActions()
 {
-	m_Actions = m_State.GetLegalMoves();
+	std::vector<Action> actions = m_State->GetLegalMoves();
+	for (const auto& action : actions)
+		m_Actions.emplace_back(std::make_unique<Action>(action));
 }
 
 void MinimaxAI::Node::populateChildren()
 {
 	for (const auto& action : m_Actions)
 	{
-		Simulator simulator(m_State, action);
-		m_Children.emplace_back(Node(simulator.GenerateNewState(action)));
+		Simulator simulator(*m_State, *action);
+		auto state = simulator.GenerateNewState(*action);
+		auto ptr = std::make_unique<Node>(state);
+		m_Children.emplace_back(std::move(ptr));
 	}
 }
--- TurnBasedAICPP/AI/MinimaxAI.h
+++ TurnBasedAICPP/AI/MinimaxAI.h
@@ -10,8 +10,8 @@ class MinimaxAI
 public:
 	explicit MinimaxAI(const GameState& state)
 		:m_State(state) {}
-	Action GetMove();
-
+	Action GetAction() const override;
+	
 private:
 	const GameState& m_State;
 
@@ -20,15 +20,15 @@ private:
 	class Node {
 	public: 
 		explicit Node(const GameState& state)
-			: m_State(state), m_Evaluation(getEvaluation()) {}
-		int Minimax(int depth, int alpha, int beta, bool maximiser);
+			: m_State(std::make_unique<GameState>(state)) {}
+		int Minimax(int depth, bool maximiser, int alpha = INT32_MIN, int beta = INT32_MAX);
 		int GetEvaluation() const { return m_Evaluation; }
 		Action GetBestAction() const { return m_BestAction; }
 	private:
-		const GameState& m_State;
-		std::vector<Action> m_Actions;
-		std::vector<Node> m_Children;
-		int m_Evaluation;
+		const std::unique_ptr<GameState> m_State;
+		std::vector<std::unique_ptr<Action>> m_Actions;
+		std::vector<std::unique_ptr<Node>> m_Children;
+		int m_Evaluation = 0;
 		Action m_BestAction;
 
 		int getEvaluation() const;
