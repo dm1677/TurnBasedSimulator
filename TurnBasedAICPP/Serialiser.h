6d1d9989d2d17a1514f2583117bd4b96509e0ebf
Dan <dan@manby.org>
2023-11-15 14:39:46 +0000

    Added convenience functions for reading and writing GameStates to/from file.

--- TurnBasedAICPP/Serialiser.h
+++ TurnBasedAICPP/Serialiser.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "GameSimulator/Unit.h"
 #include "GameSimulator/Action.h"
+#include "GameSimulator/GameState.h"
 #include <string>
 #include <vector>
 
@@ -10,7 +11,9 @@ public:
 	void WriteUnitToFile(const Unit& unit, const std::string& filename) const;
 	void ReadUnitFromFile(const std::string& filename) const;
 	void WriteUnitsToBinaryFile(const std::vector<Unit>& units, const std::string& filename) const;
+	void WriteStateToFile(const GameState& state, const std::string& filename) const { WriteUnitsToBinaryFile(state.GetUnitData(), filename); }
 	std::vector<Unit> ReadUnitsFromBinaryFile(const std::string& filename) const;
+	GameState ReadStateFromFile(const std::string& filename) const { auto units = ReadUnitsFromBinaryFile(filename); return GameState(units, Player); } //TODO: allow encoding of player to move
 	void WriteReplayToFile(const std::vector<Action>& actions, const std::string& filename) const;
 	std::vector<Action> ReadReplayFile(const std::string& filename) const;
 private:
